3fe17807af62e7d3b314db95d509e091f133d051
Dominik Frey <domfr93@liu.se>
2025-03-19 16:43:25 +0100

    gen

--- subprojects/generator/src/test/java/tools/refinery/generator/ModelGenerationTest.java
+++ subprojects/generator/src/test/java/tools/refinery/generator/ModelGenerationTest.java
@@ -25,8 +25,11 @@ class ModelGenerationTest {
 
 	@Test
 	void modelGenerationTest() throws IOException{
-		String inputPath = "C:\\Users\\freyd\\Desktop\\MSC_3\\Dipterv\\refinery\\subprojects\\generator\\src\\test" +
-				"\\resources\\ASVScenarioV2.problem";
+		//String inputPath = "C:\\Users\\freyd\\Desktop\\MSC_3\\Dipterv\\refinery\\subprojects\\generator\\src\\test" +
+		//		"\\resources\\ASVScenario.problem";
+
+		String inputPath = "C:\\Users\\domfr93\\Desktop\\refinery\\subprojects\\generator\\src\\test\\resources" +
+				"\\ASVScenario.problem";
 
 		var problem = loader.loadFile(inputPath);
 		generatorFactory.partialInterpretationBasedNeighborhoods(true);
